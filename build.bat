5c235f18e03067d6f5157c38640d6a9369619fdb
fahadasq <fahad.a.s.algarni@gmail.com>
2025-02-01 20:21:58 +0300

    Initial commit

---
+++ build.bat
@@ -0,0 +1,1 @@
+@g++ pong.cpp -o pong.exe -I include -L lib -l raylib -l gdi32 -l opengl32 -l winmm
---
+++ pong.cpp
@@ -0,0 +1,13 @@
+#include <raylib.h>
+
+int main()
+{
+	InitWindow(1280, 720, "Pong");
+
+	while (!WindowShouldClose())
+	{
+		ClearBackground(BLACK);
+		BeginDrawing();
+		EndDrawing();
+	}
+}
